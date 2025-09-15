5ec90f2a2eed1b520ffd65119fa2d5bc69f937a4
Danh Nguyễn <danh010500@gmail.com>
2025-09-15 21:41:18 +0700

    Fix

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,21 @@ import { AppModule } from './app.module';
 import 'dotenv/config';
 import "reflect-metadata";
 
+import * as fs from 'fs';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/key.pem'),
+    cert: fs.readFileSync('./secrets/cert.pem'),
+  };
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
+
   app.enableCors({
     origin: 'https://kidoedu.vn',
     credentials: true,
   });
-  await app.listen(3000, '0.0.0.0');
-}
 
+  await app.listen(443, '0.0.0.0'); // HTTPS mặc định chạy port 443
+}
 bootstrap();
