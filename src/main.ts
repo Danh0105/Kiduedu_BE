039858337ce4c3db3af8344cff371695029d3ac0
Danh Nguyễn <danh010500@gmail.com>
2025-09-15 15:24:55 +0700

    update 15/09/2025

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import "reflect-metadata";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'https://kidu-kq2b3mm22-danh0105s-projects.vercel.app',
     credentials: true,
   });
   await app.listen(3000, '0.0.0.0');
