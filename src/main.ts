185625a5c90241b65d0d8b9687ff5e2d015c475a
Danh Nguyễn <danh010500@gmail.com>
2025-09-15 21:10:13 +0700

    Fix white screen issue on Vercel

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ import "reflect-metadata";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://kidu-edu.vercel.app',
+    origin: 'https://kidoedu.vn',
     credentials: true,
   });
   await app.listen(3000, '0.0.0.0');
-
 }
+
 bootstrap();
