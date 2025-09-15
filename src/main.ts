c95a1c2851c378c5871914f3cf16728284ca7aea
Danh Nguyễn <danh010500@gmail.com>
2025-09-15 15:34:57 +0700

    update 15/09/2025

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import "reflect-metadata";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://kidu-kq2b3mm22-danh0105s-projects.vercel.app',
+    origin: 'https://kidu-edu.vercel.app',
     credentials: true,
   });
   await app.listen(3000, '0.0.0.0');
