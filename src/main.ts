1a55586d6cc6a165a125af9ac390cfa016c6fac9
Danh Nguyễn <danh010500@gmail.com>
2025-09-16 10:38:11 +0700

    Fix main

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,12 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ['https://kidoedu.vn', 'http://localhost:3000'],
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
-    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: ['https://kidoedu.vn', 'http://localhost:3000'],
+  //   methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  //   allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
+  //   credentials: true,
+  // });
   await app.listen(3000);
 
 }
