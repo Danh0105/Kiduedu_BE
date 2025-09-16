2cce53ea873704ae78b4cd061c327d986603c051
Danh Nguyễn <danh010500@gmail.com>
2025-09-16 08:58:44 +0700

    Fix main

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://kidoedu.vn',
+    origin: ['https://kidoedu.vn', 'http://localhost:3000'],
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
     credentials: true,
