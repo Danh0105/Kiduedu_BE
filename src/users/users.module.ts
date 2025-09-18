f101ce6937f5a87a52acf9d9c43176b72f3aa8d9
Danh Nguyễn <danh010500@gmail.com>
2025-09-18 16:59:32 +0700

    fix registe

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
+import { Cart } from 'src/cart/entities/cart.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Cart])],
   providers: [UsersService],
   exports: [UsersService],
 })
