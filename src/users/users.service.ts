41031130fc35b6bd4db636ec6eda307d035f881c
Danh Nguyễn <danh010500@gmail.com>
2025-09-18 16:39:02 +0700

    fix registe

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,12 +3,15 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { Cart } from 'src/cart/entities/cart.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
+    @InjectRepository(Cart)
+    private cartRepository: Repository<Cart>,
   ) { }
 
   async findByEmail(email: string): Promise<User | null> {
@@ -32,6 +35,12 @@ export class UsersService {
       phone_number: data.phoneNumber,
       role: data.role ?? 'customer',
     });
+    const savedUser = await this.usersRepository.save(user);
+
+    const cart = this.cartRepository.create({
+      user: savedUser,
+    });
+    await this.cartRepository.save(cart);
     return this.usersRepository.save(user);
   }
 }
