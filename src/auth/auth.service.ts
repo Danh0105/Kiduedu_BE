6c761bcd28dc3ef42a9130ed1e6939389019a88d
Danh Nguyễn <danh010500@gmail.com>
2025-09-19 09:21:59 +0700

    fix jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -24,10 +24,9 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { email: user.email, sub: user.userId, role: user.role };
+    const payload = { email: user.email, sub: user.user_id, role: user.role };
     return {
       access_token: this.jwtService.sign(payload),
-      role: user.role,
     };
   }
 
