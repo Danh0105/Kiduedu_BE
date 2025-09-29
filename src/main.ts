d79db5f3c11329ee7bfa74a8b06395a02f4fdc84
Danh Nguyễn <danh010500@gmail.com>
2025-09-29 10:11:24 +0700

    port

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://kidoedu.edu.vn', 'http://localhost:8028'],
+    origin: ['https://kidoedu.edu.vn', 'http://localhost:8082'],
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
     credentials: true,
   });
-  await app.listen(8028);
+  await app.listen(8082);
 
 }
 bootstrap();
