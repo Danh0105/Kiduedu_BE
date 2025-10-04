b60dee79a7d47319be00fe7fe40ebb160cfcbcde
Danh Nguyễn <danh010500@gmail.com>
2025-10-04 09:14:17 +0700

    update FTS

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://kidoedu.edu.vn', 'http://localhost:8082', 'http://localhost:3000'],
+    origin: ['https://kidoedu.edu.vn', 'http://localhost:8082', 'http://localhost:3000', 'https://www.kidoedu.edu.vn', 'https://kidoedu.vn'],
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
     credentials: true,
