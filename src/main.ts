26216c322c4b660c588286ba1e4383a57c12a28f
Danh Nguyễn <danh010500@gmail.com>
2025-10-04 09:39:09 +0700

    update FTS

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,14 @@ import "reflect-metadata";
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
-
+  app.setGlobalPrefix('api');
   app.enableCors({
     origin: ['https://kidoedu.edu.vn', 'http://localhost:8082', 'http://localhost:3000', 'https://www.kidoedu.edu.vn', 'https://kidoedu.vn'],
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
     credentials: true,
   });
-  await app.listen(3000);
+  await app.listen(3000, '127.0.0.1');
 
 }
 bootstrap();
