9799057490d545bebc7b081a7fb1e6cce7f003d3
Danh0105 <danh010500@gmail.com>
2025-10-21 23:29:06 +0700

    1105211025

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,24 @@ import "reflect-metadata";
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
+  const whitelist = [
+    'https://www.kidoedu.edu.vn',
+    'https://kidoedu.vn',
+    'http://localhost:3001',   // nếu cần dev
+  ];
+
   app.enableCors({
-    origin: '*',
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
-    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
-    credentials: true,
+    origin: (origin, cb) => {
+      // Cho phép non-browser (Postman) origin null
+      if (!origin) return cb(null, true);
+      cb(null, whitelist.includes(origin));
+    },
+    credentials: true, // 👈 bật
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+    exposedHeaders: ['Content-Disposition'], // nếu cần tải file
   });
+
   await app.listen(3000, '0.0.0.0');
 
 
