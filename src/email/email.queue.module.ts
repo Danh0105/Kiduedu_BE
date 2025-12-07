56f5200b55d3382cd5831e7f6922dfb13083531a
phuocyota <nhphuoca7tqtpy@gmail.com>
2025-12-07 17:34:55 +0700

    503120725

--- src/email/email.queue.module.ts
+++ src/email/email.queue.module.ts
@@ -11,8 +11,8 @@ import { SettingsModule } from 'src/settings/settings.module';
         BullModule.registerQueue({
             name: 'emailQueue',
             connection: {
-                host: '127.0.0.1',
-                port: 6379,
+                host: process.env.REDIS_HOST,
+                port: Number(process.env.REDIS_PORT),
             },
         }),
     ],
