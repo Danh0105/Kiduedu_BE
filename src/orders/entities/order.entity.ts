49d4bca11a796fd5fe2b9f5f414e3bb7cd5504f3
Danh0105 <danh010500@gmail.com>
2026-01-21 10:19:37 +0700

    vtb update

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -121,13 +121,10 @@ export class Order {
     | 'Failed'
     | 'Cancelled'
     | 'Expired';
-  @Column({ type: 'varchar', length: 50, nullable: true })
+  @Column({ name: 'trans_id', type: 'varchar', length: 50, nullable: true })
   transId: string | null;
 
-  /**
-   * bankTransId: ID giao dịch tại ngân hàng
-   * BẮT BUỘC unique
-   */
-  @Column({ type: 'varchar', length: 50, nullable: true, unique: true })
+  @Column({ name: 'bank_trans_id', type: 'varchar', length: 50, nullable: true, unique: true })
   bankTransId: string | null;
+
 }
