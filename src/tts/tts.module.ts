b31df4690c876371cdbefc4b70b2b99305ff5194
Danh0105 <danh010500@gmail.com>
2026-01-23 13:08:17 +0700

    1011

--- src/tts/tts.module.ts
+++ src/tts/tts.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { TtsService } from "./tts.service";
 import { TtsController } from "./tts.controller";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
+    imports: [ConfigModule],
     providers: [TtsService],
     controllers: [TtsController],
 })
